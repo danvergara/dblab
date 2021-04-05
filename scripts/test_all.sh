345fc800766e96af387ff69e70c126dc58730dcb
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2021-04-04 21:41:17 -0500

    test(scripts): add a section to run integration tests against mysql database containers

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -11,6 +11,7 @@ export PGUSER="postgres"
 export DBPASSWORD="password"
 export DATABASE="users"
 export PGPORT="15432"
+export MYSQL_PORT="13306"
 
 # Test different versions of postgres available on Docker Hub.
 pgversions="9.6 10.16 11.11 12.6 13.2"
@@ -25,4 +26,20 @@ do
 		sleep 5
 		make test
 	echo "--------------END POSTGRES TESTS-------------"
+done
+
+
+# Test different versions of mysql available on Docker Hub.
+mysql_versions="5.6 5.7 8.0"
+
+for i in $mysql_versions
+do
+	MYSQL_VERSION="$i"
+	echo "--------------BEGIN MYSQL TESTS-------------"
+	echo "Running test against MySQL v$MYSQL_VERSION"
+	  docker rm -f mysql || true
+	  docker run -p $MYSQL_PORT:3306 --name mysql -e MYSQL_PASSWORD=$DBPASSWORD -d mysql:$MYSQL_VERSION
+		sleep 5
+		make test
+	echo "--------------END MYSQL TESTS-------------"
 done
