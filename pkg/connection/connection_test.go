45636c79ea7c4986140ada74a089691e9d36ff2d
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2021-04-21 00:33:07 -0500

    test(connection): add test cases to test connection strings validation

--- pkg/connection/connection_test.go
+++ pkg/connection/connection_test.go
@@ -22,6 +22,171 @@ func TestBuildConnectionFromOpts(t *testing.T) {
 		given given
 		want  want
 	}{
+		// postgres
+		{
+			name: "valid postgres localhost",
+			given: given{
+				opts: command.Options{
+					Driver: "postgres",
+					URL:    "postgres://user:password@localhost:5432/db?sslmode=disable",
+				},
+			},
+			want: want{
+				uri: "postgres://user:password@localhost:5432/db?sslmode=disable",
+			},
+		},
+		{
+			name: "valid postgres localhost but add sslmode",
+			given: given{
+				opts: command.Options{
+					Driver: "postgres",
+					URL:    "postgres://user:password@localhost:5432/db",
+				},
+			},
+			want: want{
+				uri: "postgres://user:password@localhost:5432/db?sslmode=disable",
+			},
+		},
+		{
+			name: "valid postgres localhost postgresql as protocol",
+			given: given{
+				opts: command.Options{
+					Driver: "postgres",
+					URL:    "postgresql://user:password@localhost:5432/db",
+				},
+			},
+			want: want{
+				uri: "postgresql://user:password@localhost:5432/db?sslmode=disable",
+			},
+		},
+		{
+			name: "error misspelled postgres",
+			given: given{
+				opts: command.Options{
+					Driver: "postgres",
+					URL:    "potgre://user:password@localhost:5432/db",
+				},
+			},
+			want: want{
+				hasError: true,
+				err:      ErrInvalidUPostgresRLFormat,
+			},
+		},
+		{
+			name: "error invalid url",
+			given: given{
+				opts: command.Options{
+					Driver: "postgres",
+					URL:    "not-a-url",
+				},
+			},
+			want: want{
+				hasError: true,
+				err:      ErrInvalidUPostgresRLFormat,
+			},
+		},
+		// mysql
+		{
+			name: "valid mysql localhost",
+			given: given{
+				opts: command.Options{
+					Driver: "mysql",
+					URL:    "mysql://user:password@tcp(localhost:3306)/db",
+				},
+			},
+			want: want{
+				uri: "mysql://user:password@tcp(localhost:3306)/db",
+			},
+		},
+		{
+			name: "valid mysql localhost with params",
+			given: given{
+				opts: command.Options{
+					Driver: "mysql",
+					URL:    "mysql://user:password@tcp(localhost:3306)/db?charset=utf8",
+				},
+			},
+			want: want{
+				uri: "mysql://user:password@tcp(localhost:3306)/db?charset=utf8",
+			},
+		},
+		{
+			name: "valid mysql remote url",
+			given: given{
+				opts: command.Options{
+					Driver: "mysql",
+					URL:    "mysql://user:password@tcp(your-amazonaws-uri.com:3306)/dbname",
+				},
+			},
+			want: want{
+				uri: "mysql://user:password@tcp(your-amazonaws-uri.com:3306)/dbname",
+			},
+		},
+		{
+			name: "error misspelled mysql",
+			given: given{
+				opts: command.Options{
+					Driver: "mysql",
+					URL:    "mysq://user:password@tcp(localhost:3306)/db?charset=utf8",
+				},
+			},
+			want: want{
+				hasError: true,
+				err:      ErrInvalidUMySQLRLFormat,
+			},
+		},
+		{
+			name: "error invalid url",
+			given: given{
+				opts: command.Options{
+					Driver: "mysql",
+					URL:    "not-a-url",
+				},
+			},
+			want: want{
+				hasError: true,
+				err:      ErrInvalidUMySQLRLFormat,
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			uri, err := BuildConnectionFromOpts(tc.given.opts)
+
+			if tc.want.hasError {
+				assert.Error(t, err)
+
+				if !errors.Is(err, tc.want.err) {
+					t.Errorf("got %v, wanted %v", err, tc.want.err)
+				}
+
+				return
+			}
+
+			assert.NoError(t, err)
+			assert.Equal(t, tc.want.uri, uri)
+		})
+	}
+}
+
+func TestFormatPostgresURL(t *testing.T) {
+	type given struct {
+		opts command.Options
+	}
+	type want struct {
+		uri      string
+		hasError bool
+		err      error
+	}
+	var cases = []struct {
+		name  string
+		given given
+		want  want
+	}{
 		{
 			name: "valid postgres localhost",
 			given: given{
@@ -90,44 +255,6 @@ func TestBuildConnectionFromOpts(t *testing.T) {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
-
-			uri, err := BuildConnectionFromOpts(tc.given.opts)
-
-			if tc.want.hasError {
-				assert.Error(t, err)
-
-				if !errors.Is(err, tc.want.err) {
-					t.Errorf("got %v, wanted %v", err, tc.want.err)
-				}
-
-				return
-			}
-
-			assert.NoError(t, err)
-			assert.Equal(t, tc.want.uri, uri)
-		})
-	}
-}
-
-func TestFormatPostgresURL(t *testing.T) {
-	type given struct {
-		opts command.Options
-	}
-	type want struct {
-		uri      string
-		hasError bool
-		err      error
-	}
-	var cases = []struct {
-		name  string
-		given given
-		want  want
-	}{}
-
-	for _, tc := range cases {
-		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
 			uri, err := formatPostgresURL(tc.given.opts)
 
 			if tc.want.hasError {
@@ -159,12 +286,76 @@ func TestFormatMySQLURL(t *testing.T) {
 		name  string
 		given given
 		want  want
-	}{}
+	}{
+		{
+			name: "valid mysql localhost",
+			given: given{
+				opts: command.Options{
+					Driver: "mysql",
+					URL:    "mysql://user:password@tcp(localhost:3306)/db",
+				},
+			},
+			want: want{
+				uri: "mysql://user:password@tcp(localhost:3306)/db",
+			},
+		},
+		{
+			name: "valid mysql localhost with params",
+			given: given{
+				opts: command.Options{
+					Driver: "mysql",
+					URL:    "mysql://user:password@tcp(localhost:3306)/db?charset=utf8",
+				},
+			},
+			want: want{
+				uri: "mysql://user:password@tcp(localhost:3306)/db?charset=utf8",
+			},
+		},
+		{
+			name: "valid mysql remote url",
+			given: given{
+				opts: command.Options{
+					Driver: "mysql",
+					URL:    "mysql://user:password@tcp(your-amazonaws-uri.com:3306)/dbname",
+				},
+			},
+			want: want{
+				uri: "mysql://user:password@tcp(your-amazonaws-uri.com:3306)/dbname",
+			},
+		},
+		{
+			name: "error misspelled mysql",
+			given: given{
+				opts: command.Options{
+					Driver: "mysql",
+					URL:    "mysq://user:password@tcp(localhost:3306)/db?charset=utf8",
+				},
+			},
+			want: want{
+				hasError: true,
+				err:      ErrInvalidUMySQLRLFormat,
+			},
+		},
+		{
+			name: "error invalid url",
+			given: given{
+				opts: command.Options{
+					Driver: "mysql",
+					URL:    "not-a-url",
+				},
+			},
+			want: want{
+				hasError: true,
+				err:      ErrInvalidUMySQLRLFormat,
+			},
+		},
+	}
 
 	for _, tc := range cases {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
+
 			uri, err := formatMySQLURL(tc.given.opts)
 
 			if tc.want.hasError {
