d89aa0d94cb0973a1497290c15327ca542d464d9
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2021-04-25 22:15:05 -0500

    feat(gui): add a package called to gui to manage all related to the gui app

--- go.mod
+++ go.mod
@@ -5,8 +5,11 @@ go 1.16
 require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/jmoiron/sqlx v1.3.3 // indirect
+	github.com/jroimartin/gocui v0.4.0 // indirect
 	github.com/lib/pq v1.10.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/nsf/termbox-go v1.1.0 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.7.0
---
+++ pkg/gui/layout.go
@@ -0,0 +1,55 @@
+package gui
+
+import (
+	"github.com/jroimartin/gocui"
+	"github.com/olekukonko/tablewriter"
+)
+
+// Layout is called for every screen re-render e.g. when the screen is resized
+func Layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+
+	if v, err := g.SetView("tables", 0, 0, int(0.2*float32(maxX)), maxY-5); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+
+		v.Title = "Tables"
+	}
+
+	if v, err := g.SetView("query", int(0.2*float32(maxX)), 0, maxX, maxY-40); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+
+		v.Title = "SQL Query"
+
+		v.Editable = true
+		v.Wrap = true
+
+		if _, err := g.SetCurrentView("query"); err != nil {
+			return err
+		}
+	}
+
+	if v, err := g.SetView("rows", int(0.2*float32(maxX)), maxY-40, maxX, maxY-5); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+
+		// init table
+		table := tablewriter.NewWriter(v)
+		table.SetCenterSeparator("|")
+		table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
+		table.Render()
+
+		v.Title = "Rows"
+	}
+
+	return nil
+}
+
+// Quit is called to end the gui app
+func Quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
