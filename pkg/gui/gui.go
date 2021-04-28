eca91ab077202c79a237718ca85ee3dfab2a849f
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2021-04-27 23:32:31 -0500

    style(pkg): add points at the end of comments

--- pkg/gui/gui.go
+++ pkg/gui/gui.go
@@ -2,19 +2,19 @@ package gui
 
 import "github.com/jroimartin/gocui"
 
-// Gui wraps the gocui Gui object which handles rendering and events
+// Gui wraps the gocui Gui object which handles rendering and events.
 type Gui struct {
 	g *gocui.Gui
 }
 
-// New builds a new gui handler
+// New builds a new gui handler.
 func New(g *gocui.Gui) *Gui {
 	return &Gui{
 		g: g,
 	}
 }
 
-// Run setup the gui with keybindings and start the mainloop
+// Run setup the gui with keybindings and start the mainloop.
 func (gui *Gui) Run() error {
 	defer gui.g.Close()
 
