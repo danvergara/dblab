cd520e98b2f97631aded8e8080649971fb81eb15
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2021-04-27 23:29:42 -0500

    feat(app): add an app struct to handle the application

---
+++ pkg/app/app.go
@@ -0,0 +1,29 @@
+package app
+
+import (
+	"github.com/danvergara/dblab/pkg/client"
+	"github.com/danvergara/dblab/pkg/gui"
+)
+
+// App Struct
+type App struct {
+	c *client.Client
+	g *gui.Gui
+}
+
+// New bootstrap a new application
+func New(c *client.Client, g *gui.Gui) *App {
+	return &App{
+		c: c,
+		g: g,
+	}
+}
+
+// Run runs the application
+func (a *App) Run() error {
+	if err := a.g.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
