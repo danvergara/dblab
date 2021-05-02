2b64c3a00a3957565b7c1047aee081393eace08d
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2021-05-02 18:37:43 -0500

    feat(gui): add a function used to perform a select query based on the selected table

--- pkg/gui/database_helpers.go
+++ pkg/gui/database_helpers.go
@@ -107,3 +107,95 @@ func (gui *Gui) runQuery() func(g *gocui.Gui, v *gocui.View) error {
 		return nil
 	}
 }
+
+func (gui *Gui) selectTable(g *gocui.Gui, v *gocui.View) error {
+	_, cy := v.Cursor()
+
+	t, err := v.Line(cy)
+	if err != nil {
+		return err
+	}
+
+	query := fmt.Sprintf("SELECT * FROM %s;", t)
+
+	rows, err := gui.c.DB().Queryx(query)
+	if err != nil {
+		return err
+	}
+
+	// Gets the names of the columns of the result set.
+	columnNames, err := rows.Columns()
+	if err != nil {
+		return err
+	}
+
+	resultSet := [][]string{}
+
+	for rows.Next() {
+		// cols is an []interface{} of all of the column results.
+		cols, err := rows.SliceScan()
+		if err != nil {
+			return err
+		}
+
+		// Convert []interface{} into []string.
+		s := make([]string, len(cols))
+		for i, v := range cols {
+			s[i] = fmt.Sprint(v)
+		}
+
+		resultSet = append(resultSet, s)
+	}
+
+	ov, err := gui.g.View("rows")
+	if err != nil {
+		return err
+	}
+
+	// Cleans the view.
+	ov.Rewind()
+	ov.Clear()
+
+	// Setup the table.
+	table := tablewriter.NewWriter(ov)
+	table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: true})
+	table.SetHeader(columnNames)
+	// Add Bulk Data.
+	table.AppendBulk(resultSet)
+	table.Render()
+	return nil
+}
+
+func cursorUp(g *gocui.Gui, v *gocui.View) error {
+	if v != nil {
+		ox, oy := v.Origin()
+		cx, cy := v.Cursor()
+
+		if err := v.SetCursor(cx, cy-1); err != nil && oy > 0 {
+			if err := v.SetOrigin(ox, oy-1); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func cursorDown(g *gocui.Gui, v *gocui.View) error {
+	if v != nil {
+		cx, cy := v.Cursor()
+
+		l, err := v.Line(cy + 1)
+		if err != nil {
+			return err
+		}
+		if l != "" {
+			if err := v.SetCursor(cx, cy+1); err != nil {
+				ox, oy := v.Origin()
+				if err := v.SetOrigin(ox, oy+1); err != nil {
+					return err
+				}
+			}
+		}
+	}
+	return nil
+}
