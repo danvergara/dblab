55c0d1d5799344f4e3bbcb14b08c6cf1cc77d745
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2021-05-07 21:38:13 -0500

    refactor(client): extract the query logic from the gui and add it to the client

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"fmt"
+
 	// mysql driver.
 	_ "github.com/go-sql-driver/mysql"
 	// postgres driver.
@@ -34,6 +36,48 @@ func New(opts command.Options) (*Client, error) {
 	}
 
 	return &c, nil
+}
+
+// Query returns performs the query and returns the result set and the colum names.
+func (c *Client) Query(q string) ([][]string, []string, error) {
+	resultSet := [][]string{}
+
+	// Runs the query extracting the content of the view calling the Buffer method.
+	rows, err := c.db.Queryx(q)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Gets the names of the columns of the result set.
+	columnNames, err := rows.Columns()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	for rows.Next() {
+		// cols is an []interface{} of all of the column results.
+		cols, err := rows.SliceScan()
+		if err != nil {
+			return nil, nil, err
+		}
+
+		// Convert []interface{} into []string.
+		s := make([]string, len(cols))
+		for i, v := range cols {
+			s[i] = fmt.Sprint(v)
+		}
+
+		resultSet = append(resultSet, s)
+	}
+
+	return resultSet, columnNames, nil
+}
+
+// TableContent returns all the rows of a table.
+func (c *Client) TableContent(tableName string) ([][]string, []string, error) {
+	query := fmt.Sprintf("SELECT * FROM %s;", tableName)
+
+	return c.Query(query)
 }
 
 // DB Return the db attribute.
