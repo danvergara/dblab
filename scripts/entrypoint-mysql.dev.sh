1883247e131919e5ceccfbbdcb6d2fbd2800f71e
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2021-05-18 21:44:46 -0500

    build(docker): add a mysql container and other dblab container to run migrations and seeds on the mysql container

---
+++ scripts/entrypoint-mysql.dev.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+echo "Waiting for postgres..."
+
+while ! nc -z mysql 3306; do
+  sleep 0.1
+done
+
+echo "MySQL started"
+
+echo "Runinng the migrations against the DB"
+go run cmd/dbmigrate/main.go
+
+echo "Seeding the database"
+go run cmd/seeder/main.go seed
