1987b4947537faaa5eda1e6492398691d6c56494
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2021-06-13 19:52:08 -0500

    test(config): add test function to the init function
    34

---
+++ pkg/config/config_test.go
@@ -0,0 +1,20 @@
+package config_test
+
+import (
+	"testing"
+
+	"github.com/danvergara/dblab/pkg/config"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInit(t *testing.T) {
+	opts, err := config.Init()
+
+	assert.NoError(t, err)
+	assert.Equal(t, "localhost", opts.Host)
+	assert.Equal(t, "5432", opts.Port)
+	assert.Equal(t, "users", opts.DBName)
+	assert.Equal(t, "postgres", opts.User)
+	assert.Equal(t, "password", opts.Pass)
+	assert.Equal(t, "postgres", opts.Driver)
+}
