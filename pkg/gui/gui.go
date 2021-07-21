1ef4bcbab4d4c5d6e93a1a8320fefb1f61d484bd
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2021-07-21 11:45:30 -0500

    feat(gui): add the new color frame feature from gocui

--- pkg/gui/gui.go
+++ pkg/gui/gui.go
@@ -28,7 +28,7 @@ func (gui *Gui) Run() error {
 	gui.g.Highlight = true
 	gui.g.Cursor = true
 	gui.g.Mouse = true
-	gui.g.SelFgColor = gocui.ColorGreen
+	gui.g.SelFrameColor = gocui.ColorGreen
 
 	gui.g.SetManagerFunc(gui.layout)
 
