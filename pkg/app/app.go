35c9b0833ff84c11f0be7aa0bbf311a15de5a6b3
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2021-08-11 12:55:51 -0500

    refactor(app): refactor the app to manage the creation of the gui and client objects removing them from the root

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,23 +1,46 @@
 package app
 
 import (
+	"github.com/danvergara/dblab/pkg/client"
+	"github.com/danvergara/dblab/pkg/command"
 	"github.com/danvergara/dblab/pkg/gui"
+	"github.com/danvergara/gocui"
 )
 
 // App Struct.
 type App struct {
 	g *gui.Gui
+	c *client.Client
 }
 
 // New bootstrap a new application.
-func New(g *gui.Gui) *App {
-	return &App{
-		g: g,
+func New(g *gocui.Gui, opts command.Options) (*App, error) {
+	c, err := client.New(opts)
+	if err != nil {
+		return nil, err
 	}
+
+	gcui, err := gui.New(g, c)
+	if err != nil {
+		return nil, err
+	}
+
+	app := App{
+		g: gcui,
+		c: c,
+	}
+
+	return &app, nil
 }
 
 // Run runs the application.
 func (a *App) Run() error {
+
+	defer func() {
+		_ = a.c.DB().Close()
+		a.g.Gui().Close()
+	}()
+
 	if err := a.g.Run(); err != nil {
 		return err
 	}
