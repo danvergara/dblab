3fa5139f6c034eba592442c73da28752ba3c6bf9
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2021-08-26 23:47:19 -0500

    feat(gui): add the ability to navigate on the constraints and structures views

--- pkg/gui/keybindings.go
+++ pkg/gui/keybindings.go
@@ -12,173 +12,134 @@ type keyBinding struct {
 	handler func(*gocui.Gui, *gocui.View) error
 }
 
-var keybindings []keyBinding = []keyBinding{
-	{
-		view:    "query",
-		key:     gocui.KeyCtrlH,
-		mod:     gocui.ModNone,
-		handler: nextView("query", "tables"),
-	},
-	{
-		view:    "tables",
-		key:     gocui.KeyCtrlL,
-		mod:     gocui.ModNone,
-		handler: nextView("tables", "query"),
-	},
-	{
-		view:    "query",
-		key:     gocui.KeyCtrlJ,
-		mod:     gocui.ModNone,
-		handler: nextView("query", "rows"),
-	},
-	{
-		view:    "rows",
-		key:     gocui.KeyCtrlK,
-		mod:     gocui.ModNone,
-		handler: nextView("rows", "query"),
-	},
-	{
-		view:    "rows",
-		key:     gocui.KeyCtrlH,
-		mod:     gocui.ModNone,
-		handler: nextView("rows", "tables"),
-	},
-	{
-		view:    "structure",
-		key:     gocui.KeyCtrlH,
-		mod:     gocui.ModNone,
-		handler: nextView("structure", "tables"),
-	},
-	{
-		view:    "structure",
-		key:     gocui.KeyCtrlK,
-		mod:     gocui.ModNone,
-		handler: nextView("structure", "query"),
-	},
-	{
-		view:    "constraints",
-		key:     gocui.KeyCtrlH,
-		mod:     gocui.ModNone,
-		handler: nextView("constraints", "tables"),
-	},
-	{
-		view:    "constraints",
-		key:     gocui.KeyCtrlK,
-		mod:     gocui.ModNone,
-		handler: nextView("constraints", "query"),
-	},
-	{
-		view:    "tables",
-		key:     'k',
-		mod:     gocui.ModNone,
-		handler: moveCursorVertically("up"),
-	},
-	{
-		view:    "tables",
-		key:     gocui.KeyArrowUp,
-		mod:     gocui.ModNone,
-		handler: moveCursorVertically("up"),
-	},
-	{
-		view:    "tables",
-		key:     'j',
-		mod:     gocui.ModNone,
-		handler: moveCursorVertically("down"),
-	},
-	{
-		view:    "tables",
-		key:     gocui.KeyArrowDown,
-		mod:     gocui.ModNone,
-		handler: moveCursorVertically("down"),
-	},
-	{
-		view:    "rows",
-		key:     gocui.KeyArrowUp,
-		mod:     gocui.ModNone,
-		handler: moveCursorVertically("up"),
-	},
-	{
-		view:    "rows",
-		key:     'k',
-		mod:     gocui.ModNone,
-		handler: moveCursorVertically("up"),
-	},
-	{
-		view:    "rows",
-		key:     gocui.KeyArrowDown,
-		mod:     gocui.ModNone,
-		handler: moveCursorVertically("down"),
-	},
-	{
-		view:    "rows",
-		key:     'j',
-		mod:     gocui.ModNone,
-		handler: moveCursorVertically("down"),
-	},
-	{
-		view:    "rows",
-		key:     gocui.KeyArrowRight,
-		mod:     gocui.ModNone,
-		handler: moveCursorHorizontally("right"),
-	},
-	{
-		view:    "rows",
-		key:     'l',
-		mod:     gocui.ModNone,
-		handler: moveCursorHorizontally("right"),
-	},
-	{
-		view:    "rows",
-		key:     gocui.KeyArrowLeft,
-		mod:     gocui.ModNone,
-		handler: moveCursorHorizontally("left"),
-	},
-	{
-		view:    "rows",
-		key:     'h',
-		mod:     gocui.ModNone,
-		handler: moveCursorHorizontally("left"),
-	},
-	{
-		view:    "constraints",
-		key:     gocui.KeyCtrlF,
-		mod:     gocui.ModNone,
-		handler: setViewOnTop("constraints", "rows"),
-	},
-	{
-		view:    "rows",
-		key:     gocui.KeyCtrlF,
-		mod:     gocui.ModNone,
-		handler: setViewOnTop("rows", "constraints"),
-	},
-	{
-		view:    "structure",
-		key:     gocui.KeyCtrlS,
-		mod:     gocui.ModNone,
-		handler: setViewOnTop("structure", "rows"),
-	},
-	{
-		view:    "rows",
-		key:     gocui.KeyCtrlS,
-		mod:     gocui.ModNone,
-		handler: setViewOnTop("rows", "structure"),
-	},
-	{
-		view:    "navigation",
-		key:     gocui.MouseLeft,
-		mod:     gocui.ModNone,
-		handler: navigation,
-	},
-	{
-		view:    "",
-		key:     gocui.KeyCtrlC,
-		mod:     gocui.ModNone,
-		handler: quit,
-	},
+// initialKeyBindings returns an slice with the standard key bindings.
+func initialKeyBindings() []keyBinding {
+	bindings := []keyBinding{
+		{
+			view:    "query",
+			key:     gocui.KeyCtrlH,
+			mod:     gocui.ModNone,
+			handler: nextView("query", "tables"),
+		},
+		{
+			view:    "tables",
+			key:     gocui.KeyCtrlL,
+			mod:     gocui.ModNone,
+			handler: nextView("tables", "query"),
+		},
+		{
+			view:    "query",
+			key:     gocui.KeyCtrlJ,
+			mod:     gocui.ModNone,
+			handler: nextView("query", "rows"),
+		},
+		{
+			view:    "rows",
+			key:     gocui.KeyCtrlK,
+			mod:     gocui.ModNone,
+			handler: nextView("rows", "query"),
+		},
+		{
+			view:    "rows",
+			key:     gocui.KeyCtrlH,
+			mod:     gocui.ModNone,
+			handler: nextView("rows", "tables"),
+		},
+		{
+			view:    "structure",
+			key:     gocui.KeyCtrlH,
+			mod:     gocui.ModNone,
+			handler: nextView("structure", "tables"),
+		},
+		{
+			view:    "structure",
+			key:     gocui.KeyCtrlK,
+			mod:     gocui.ModNone,
+			handler: nextView("structure", "query"),
+		},
+		{
+			view:    "constraints",
+			key:     gocui.KeyCtrlH,
+			mod:     gocui.ModNone,
+			handler: nextView("constraints", "tables"),
+		},
+		{
+			view:    "constraints",
+			key:     gocui.KeyCtrlK,
+			mod:     gocui.ModNone,
+			handler: nextView("constraints", "query"),
+		},
+		{
+			view:    "constraints",
+			key:     gocui.KeyCtrlF,
+			mod:     gocui.ModNone,
+			handler: setViewOnTop("constraints", "rows"),
+		},
+		{
+			view:    "rows",
+			key:     gocui.KeyCtrlF,
+			mod:     gocui.ModNone,
+			handler: setViewOnTop("rows", "constraints"),
+		},
+		{
+			view:    "structure",
+			key:     gocui.KeyCtrlS,
+			mod:     gocui.ModNone,
+			handler: setViewOnTop("structure", "rows"),
+		},
+		{
+			view:    "rows",
+			key:     gocui.KeyCtrlS,
+			mod:     gocui.ModNone,
+			handler: setViewOnTop("rows", "structure"),
+		},
+		{
+			view:    "navigation",
+			key:     gocui.MouseLeft,
+			mod:     gocui.ModNone,
+			handler: navigation,
+		},
+		{
+			view:    "",
+			key:     gocui.KeyCtrlC,
+			mod:     gocui.ModNone,
+			handler: quit,
+		},
+	}
+
+	// view like navigation.
+	for _, viewName := range []string{"rows", "structure", "constraints"} {
+		bindings = append(bindings, []keyBinding{
+			{view: viewName, key: 'k', mod: gocui.ModNone, handler: moveCursorVertically("up")},
+			{view: viewName, key: 'j', mod: gocui.ModNone, handler: moveCursorVertically("down")},
+			{view: viewName, key: 'l', mod: gocui.ModNone, handler: moveCursorHorizontally("right")},
+			{view: viewName, key: 'h', mod: gocui.ModNone, handler: moveCursorHorizontally("left")},
+		}...)
+	}
+
+	// arrow keys navigation.
+	for _, viewName := range []string{"rows", "structure", "constraints"} {
+		bindings = append(bindings, []keyBinding{
+			{view: viewName, key: gocui.KeyArrowUp, mod: gocui.ModNone, handler: moveCursorVertically("up")},
+			{view: viewName, key: gocui.KeyArrowDown, mod: gocui.ModNone, handler: moveCursorVertically("down")},
+			{view: viewName, key: gocui.KeyArrowRight, mod: gocui.ModNone, handler: moveCursorHorizontally("right")},
+			{view: viewName, key: gocui.KeyArrowLeft, mod: gocui.ModNone, handler: moveCursorHorizontally("left")},
+		}...)
+	}
+
+	// defines the navigation on the "tables" view.
+	bindings = append(bindings, []keyBinding{
+		{view: "tables", key: 'k', mod: gocui.ModNone, handler: moveCursorVertically("up")},
+		{view: "tables", key: gocui.KeyArrowUp, mod: gocui.ModNone, handler: moveCursorVertically("up")},
+		{view: "tables", key: 'j', mod: gocui.ModNone, handler: moveCursorVertically("down")},
+		{view: "tables", key: gocui.KeyArrowDown, mod: gocui.ModNone, handler: moveCursorVertically("down")},
+	}...)
+
+	return bindings
 }
 
 func (gui *Gui) keybindings() error {
-	for _, k := range keybindings {
+	for _, k := range initialKeyBindings() {
 		if err := gui.g.SetKeybinding(k.view, k.key, k.mod, k.handler); err != nil {
 			return err
 		}
