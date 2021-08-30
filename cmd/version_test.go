7243aafbffd25a6b2ffe2babbb0be2b872abc13c
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2021-08-30 11:47:39 -0500

    build(version): bump to version 0.12.0

--- cmd/version_test.go
+++ cmd/version_test.go
@@ -21,7 +21,7 @@ func TestVersionCmd(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if !strings.Contains(string(out), "v0.11.1") {
-		t.Fatalf("expected \"%s\" got \"%s\"", "v0.11.1", string(out))
+	if !strings.Contains(string(out), "v0.12.0") {
+		t.Fatalf("expected \"%s\" got \"%s\"", "v0.12.0", string(out))
 	}
 }
