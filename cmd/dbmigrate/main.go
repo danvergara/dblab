8384d94d9bd4db6955db8def3fc99ecf04c9a7a4
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2021-11-05 00:09:35 -0600

    build(migrations): add a cmd project to execute migrations using cobra
    49

--- cmd/dbmigrate/main.go
+++ cmd/dbmigrate/main.go
@@ -1,41 +1,22 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
-import (
-	"log"
-
-	"github.com/danvergara/dblab/pkg/config"
-	"github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/database/mysql"
-	_ "github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
-)
+import "github.com/danvergara/dblab/cmd/dbmigrate/cmd"
 
 func main() {
-	cfg := config.Get()
-
-	direction := cfg.GetMigration()
-	if direction != "down" && direction != "up" {
-		log.Println("-migrate accepts [up, down] values only")
-		return
-	}
-
-	m, err := migrate.New("file://db/migrations", cfg.GetDBConnStr())
-	if err != nil {
-		log.Printf("migration error %s", err)
-		return
-	}
-
-	if direction == "up" {
-		if err := m.Up(); err != nil {
-			log.Printf("failed migrate up: %s", err)
-			return
-		}
-	}
-
-	if direction == "down" {
-		if err := m.Down(); err != nil {
-			log.Printf("failed migrate down: %s", err)
-			return
-		}
-	}
+	cmd.Execute()
 }
