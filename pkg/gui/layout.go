3e02f9c21cb48f20dd8164f7bbe73aac5a49ade3
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2022-06-27 23:27:37 -0500

    refactor(gui): add widgets objects to abstract the creation of the app

--- pkg/gui/layout.go
+++ pkg/gui/layout.go
@@ -1,11 +1,6 @@
 package gui
 
 import (
-	"errors"
-	"fmt"
-	"strings"
-
-	"github.com/common-nighthawk/go-figure"
 	"github.com/danvergara/gocui"
 	"github.com/fatih/color"
 )
@@ -15,103 +10,28 @@ var (
 	options = []string{"Rows", "Structure", "Constraints", "Indexes"}
 )
 
-// Layout is called for every screen re-render e.g. when the screen is resized.
-func (gui *Gui) layout(g *gocui.Gui) error {
+func (gui *Gui) setLayout() {
 	maxX, maxY := gui.g.Size()
 
-	if v, err := gui.g.SetView("banner", 0, 0, int(0.19*float32(maxX)), int(0.14*float32(maxY))); err != nil {
-		if !errors.Is(err, gocui.ErrUnknownView) {
-			return err
-		}
+	banner := NewBannerWidget("banner", 0, 0, int(0.19*float32(maxX)), int(0.14*float32(maxY)), "dblab", gocui.ColorMagenta)
 
-		v.FrameColor = gocui.ColorMagenta
-		myFigure := figure.NewFigure("dblab", "", true)
-		figure.Write(v, myFigure)
-	}
+	tables := NewTableWidget("tables", 0, int(0.16*float32(maxY)), int(0.19*float32(maxX)), int(0.94*float32(maxY)), "Tables", gocui.ColorGreen, gocui.ColorBlack, gui)
 
-	if v, err := gui.g.SetView("tables", 0, int(0.16*float32(maxY)), int(0.19*float32(maxX)), int(0.94*float32(maxY))); err != nil {
-		if !errors.Is(err, gocui.ErrUnknownView) {
-			return err
-		}
+	navigation := NewNavigationWidget("navigation", int(0.2*float32(maxX)), 0, maxX-1, int(0.07*float32(maxY)), "Navigation", options)
 
-		if err := gui.showTables(); err != nil {
-			return err
-		}
+	editor := NewEditorWidget("query", int(0.2*float32(maxX)), int(0.09*float32(maxY)), maxX-1, int(0.27*float32(maxY)), "SQL Query")
+	indexes := NewOutputWidget("indexes", int(0.2*float32(maxX)), int(0.29*float32(maxY)), maxX-1, int(0.94*float32(maxY)), "Indexes", "Please select a table!")
+	constraints := NewOutputWidget("constraints", int(0.2*float32(maxX)), int(0.29*float32(maxY)), maxX-1, int(0.94*float32(maxY)), "Constraints", "Please select a table!")
+	structure := NewOutputWidget("structure", int(0.2*float32(maxX)), int(0.29*float32(maxY)), maxX-1, int(0.94*float32(maxY)), "Structure", "Please select a table!")
+	rows := NewOutputWidget("rows", int(0.2*float32(maxX)), int(0.29*float32(maxY)), maxX-1, int(0.94*float32(maxY)), "Rows", "Type the sql query above. Press Ctrl-c to quit.")
 
-		v.Title = "Tables"
-		v.Highlight = true
-		v.SelBgColor = gocui.ColorGreen
-		v.SelFgColor = gocui.ColorBlack
-	}
+	rowsPerPage := NewLabelWidget("rows-per-page", int(0.81*float32(maxX)), int(0.96*float32(maxY)), "100 rows", gocui.ColorWhite)
+	currentPage := NewLabelWidget("current-page", int(0.90*float32(maxX)), int(0.96*float32(maxY)), "01", gocui.ColorWhite)
+	slash := NewLabelWidget("slash", int(0.92*float32(maxX)), int(0.96*float32(maxY)), "/", gocui.ColorWhite)
+	totalPages := NewLabelWidget("total-pages", int(0.93*float32(maxX)), int(0.96*float32(maxY)), "50", gocui.ColorWhite)
 
-	if v, err := gui.g.SetView("navigation", int(0.2*float32(maxX)), 0, maxX-1, int(0.07*float32(maxY))); err != nil {
-		if !errors.Is(err, gocui.ErrUnknownView) {
-			return err
-		}
+	back := NewButtonWidget("back", int(0.86*float32(maxX)), int(0.96*float32(maxY)), "< BACK", gocui.ColorGreen)
+	next := NewButtonWidget("next", int(0.95*float32(maxX)), int(0.96*float32(maxY)), "NEXT >", gocui.ColorGreen)
 
-		v.Title = "Navigation"
-
-		tmpOptions := make([]string, len(options))
-		copy(tmpOptions, options)
-		tmpOptions[0] = green.Sprint("Rows")
-
-		fmt.Fprint(v, strings.Join(tmpOptions, "   "))
-	}
-
-	if v, err := gui.g.SetView("query", int(0.2*float32(maxX)), int(0.09*float32(maxY)), maxX-1, int(0.27*float32(maxY))); err != nil {
-		if !errors.Is(err, gocui.ErrUnknownView) {
-			return err
-		}
-
-		v.Title = "SQL Query"
-		v.Editable = true
-		v.Wrap = true
-		v.Highlight = true
-
-		if _, err := gui.g.SetCurrentView("query"); err != nil {
-			return err
-		}
-	}
-
-	if v, err := gui.g.SetView("indexes", int(0.2*float32(maxX)), int(0.29*float32(maxY)), maxX-1, int(0.94*float32(maxY))); err != nil {
-		if !errors.Is(err, gocui.ErrUnknownView) {
-			return err
-		}
-
-		v.Title = "Indexes"
-
-		fmt.Fprintln(v, "Please select a table!")
-	}
-
-	if v, err := gui.g.SetView("constraints", int(0.2*float32(maxX)), int(0.29*float32(maxY)), maxX-1, int(0.94*float32(maxY))); err != nil {
-		if !errors.Is(err, gocui.ErrUnknownView) {
-			return err
-		}
-
-		v.Title = "Constraints"
-
-		fmt.Fprintln(v, "Please select a table!")
-	}
-
-	if v, err := gui.g.SetView("structure", int(0.2*float32(maxX)), int(0.29*float32(maxY)), maxX-1, int(0.94*float32(maxY))); err != nil {
-		if !errors.Is(err, gocui.ErrUnknownView) {
-			return err
-		}
-
-		v.Title = "Structure"
-
-		fmt.Fprintln(v, "Please select a table!")
-	}
-
-	if v, err := gui.g.SetView("rows", int(0.2*float32(maxX)), int(0.29*float32(maxY)), maxX-1, int(0.94*float32(maxY))); err != nil {
-		if !errors.Is(err, gocui.ErrUnknownView) {
-			return err
-		}
-
-		v.Title = "Rows"
-
-		fmt.Fprintln(v, "Type the sql query above. Press Ctrl-c to quit.")
-	}
-
-	return nil
+	gui.g.SetManager(banner, tables, navigation, editor, indexes, constraints, structure, rows, rowsPerPage, back, currentPage, slash, totalPages, next)
 }
