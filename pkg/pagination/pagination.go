fdec4ea023fbd5d9cd543cbd7aa3866710b25cf7
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2022-06-30 23:59:27 -0500

    feat(pagination): add a pagination package to manage the pagination calculations

---
+++ pkg/pagination/pagination.go
@@ -0,0 +1,83 @@
+package pagination
+
+import "fmt"
+
+// Manager handles the pagination.
+type Manager struct {
+	totalPages  int
+	currentPage int
+	limit       int
+	offset      int
+}
+
+// New returns a pointer to a Manager instance.
+func New(limit, count int) (*Manager, error) {
+	m := Manager{
+		limit:       limit,
+		currentPage: 1,
+	}
+
+	m.setOffset()
+
+	if err := m.setTotalPages(count); err != nil {
+		return nil, err
+	}
+
+	return &m, nil
+}
+
+// NextPage increases the value currentPage.
+func (m *Manager) NextPage() error {
+	if m.currentPage+1 > m.totalPages {
+		return fmt.Errorf("current page should not be greater than the total pages count")
+	}
+
+	m.currentPage++
+	m.setOffset()
+
+	return nil
+}
+
+// PreviousPage decreases the value of currentPage.
+func (m *Manager) PreviousPage() error {
+	if m.currentPage-1 <= 0 {
+		return fmt.Errorf("current page should not be less than 0")
+	}
+
+	m.currentPage--
+	m.setOffset()
+
+	return nil
+}
+
+// Offset returns the limit.
+func (m *Manager) Offset() int {
+	return m.offset
+}
+
+// Limit returns the limit.
+func (m *Manager) Limit() int {
+	return m.limit
+}
+
+// setTotalPages total pages = count / limit, if the limit is greater than 0.
+func (m *Manager) setTotalPages(count int) error {
+	// limit must be greater than 0.
+	if m.limit <= 0 {
+		return fmt.Errorf("limit should be greater than 0")
+	}
+
+	m.totalPages = count / m.limit
+
+	return nil
+}
+
+// setOffset calculates the offset based of the current page and the limit.
+func (m *Manager) setOffset() {
+	m.offset = (m.currentPage - 1) * m.limit
+}
+
+// CurrentPage returns the currentPage value.
+func (m *Manager) CurrentPage() int {
+	return m.currentPage
+}
---
+++ pkg/pagination/pagination_test.go
@@ -0,0 +1,39 @@
+package pagination
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPaginationLifeCycle(t *testing.T) {
+	// This is an example of the pagination life cycle.
+	// It starts off with a limit of 50 and count from a sql query of 10000.
+	// The total pages count should be equal to count / limit = 200
+	// I'm calling the NextPage 3 times and PreviousPage two times.
+	limit := 50
+	count := 150
+
+	m, err := New(limit, count)
+	assert.NoError(t, err)
+
+	for i := 0; i < (count/m.Limit())-1; i++ {
+		_ = m.NextPage()
+	}
+
+	assert.Equal(t, count/m.Limit(), m.CurrentPage())
+	assert.Equal(t, (m.CurrentPage()-1)*limit, m.Offset())
+
+	err = m.NextPage()
+	assert.Error(t, err)
+
+	for i := 0; i < (count/m.Limit())-1; i++ {
+		_ = m.PreviousPage()
+	}
+
+	assert.Equal(t, 1, m.CurrentPage())
+	assert.Equal(t, 0, m.Offset())
+
+	err = m.PreviousPage()
+	assert.Error(t, err)
+}
