262e2155f7bca2d3b376c00a559c729503916e96
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2022-10-17 16:40:16 -0500

    build(version): bump version to v0.17.0-rc1

--- cmd/version.go
+++ cmd/version.go
@@ -32,7 +32,7 @@ func NewVersionCmd() *cobra.Command {
 		Long: `The current version of the project.
 		This projects follows the semantic versioning standard.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Fprintln(cmd.OutOrStdout(), "v0.17.0")
+			fmt.Fprintln(cmd.OutOrStdout(), "v0.17.0-rc1")
 			return nil
 		},
 	}
