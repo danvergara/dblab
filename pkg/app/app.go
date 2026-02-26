c68746e7762f2d0c7ad762c1e2b94583dafdfb4d
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2026-02-26 09:36:53 -0600

    add more boilerplate

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"github.com/danvergara/dblab/pkg/bubbletui"
 	"github.com/danvergara/dblab/pkg/client"
 	"github.com/danvergara/dblab/pkg/command"
 	"github.com/danvergara/dblab/pkg/sshdb"
@@ -12,43 +13,47 @@ type App struct {
 	t  *tui.Tui
 	c  *client.Client
 	sc *sshdb.SSHConfig
+	m  *bubbletui.Model
 }
 
 // New bootstrap a new application.
 func New(opts command.Options) (*App, error) {
-	var sc *sshdb.SSHConfig
+	// var sc *sshdb.SSHConfig
+	//
+	// if opts.SSHHost != "" {
+	// 	sc = sshdb.New(
+	// 		sshdb.WithDBDriver(opts.Driver),
+	// 		sshdb.WithSShHost(opts.SSHHost),
+	// 		sshdb.WithSShPort(opts.SSHPort),
+	// 		sshdb.WithSSHUser(opts.SSHUser),
+	// 		sshdb.WithPass(opts.SSHPass),
+	// 		sshdb.WithSSHKeyFile(opts.SSHKeyFile),
+	// 		sshdb.WithSSHKeyPass(opts.SSHKeyPassphrase),
+	// 		sshdb.WithDBDURL(opts.URL),
+	// 	)
 
-	if opts.SSHHost != "" {
-		sc = sshdb.New(
-			sshdb.WithDBDriver(opts.Driver),
-			sshdb.WithSShHost(opts.SSHHost),
-			sshdb.WithSShPort(opts.SSHPort),
-			sshdb.WithSSHUser(opts.SSHUser),
-			sshdb.WithPass(opts.SSHPass),
-			sshdb.WithSSHKeyFile(opts.SSHKeyFile),
-			sshdb.WithSSHKeyPass(opts.SSHKeyPassphrase),
-			sshdb.WithDBDURL(opts.URL),
-		)
+	// 	if err := sc.SSHTunnel(); err != nil {
+	// 		return nil, err
+	// 	}
+	// }
 
-		if err := sc.SSHTunnel(); err != nil {
-			return nil, err
-		}
-	}
+	// c, err := client.New(opts)
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	c, err := client.New(opts)
-	if err != nil {
-		return nil, err
-	}
+	// t, err := tui.New(tui.WithClient(c), tui.WithKeyBinding(&opts.TUIKeyBindings))
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	t, err := tui.New(tui.WithClient(c), tui.WithKeyBinding(&opts.TUIKeyBindings))
-	if err != nil {
-		return nil, err
-	}
+	m := bubbletui.NewModel()
 
 	app := App{
-		c:  c,
-		t:  t,
-		sc: sc,
+		// c:  c,
+		// t:  t,
+		m: &m,
+		// sc: sc,
 	}
 
 	return &app, nil
@@ -57,14 +62,14 @@ func New(opts command.Options) (*App, error) {
 // Run runs the application.
 func (a *App) Run() error {
 	defer func() {
-		if a.sc != nil {
-			_ = a.sc.Close()
-		}
+		// if a.sc != nil {
+		// _ = a.sc.Close()
+		// }
 
-		_ = a.c.DB().Close()
+		// _ = a.c.DB().Close()
 	}()
 
-	if err := a.t.Run(); err != nil {
+	if err := a.m.Run(); err != nil {
 		return err
 	}
 
--- pkg/bubbletui/bubbletui.go
+++ pkg/bubbletui/bubbletui.go
@@ -5,35 +5,88 @@ import (
 	"github.com/charmbracelet/bubbles/table"
 	"github.com/charmbracelet/bubbles/textarea"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type focusState int
 
 const (
+	// colors
+	colorGreen     = lipgloss.Color("#1fb009")
+	activeBorder   = lipgloss.Color("62")
+	inactiveBorder = lipgloss.Color("240")
+
 	focusInput focusState = iota
 	focusList
+	focusTable
 )
 
-type item struct {
-	title, desc string
-}
+var (
+	leftColumnStyle = lipgloss.NewStyle().
+			MarginRight(2).
+			Border(lipgloss.RoundedBorder())
 
-func (i item) Title() string       { return i.title }
-func (i item) Description() string { return i.desc }
-func (i item) FilterValue() string { return i.title }
+	rightTopStyle = lipgloss.NewStyle().
+			MarginBottom(1).
+			Border(lipgloss.NormalBorder())
+
+	rightBottomStyle = lipgloss.NewStyle().
+				Border(lipgloss.NormalBorder())
+
+	footerStyle = lipgloss.NewStyle().
+			Foreground(colorGreen)
+)
+
+type item string
+
+func (i item) Title() string       { return string(i) }
+func (i item) Description() string { return "" }
+func (i item) FilterValue() string { return string(i) }
 
 type Model struct {
-	input textarea.Model
-	list  list.Model
-	table table.Model
+	input   textarea.Model
+	list    list.Model
+	table   table.Model
+	focused focusState
+	width   int
+	height  int
 }
 
 func NewModel() Model {
-	var emptyItems []list.Item
+	ti := textarea.New()
+	ti.Placeholder = "Search or enter text..."
+	ti.Focus() // Start with the input focused
+
+	var items []list.Item = []list.Item{
+		item("users"),
+		item("products"),
+		item("admins"),
+		item("invoices"),
+		item("relays"),
+	}
+	l := list.New(items, list.NewDefaultDelegate(), 20, 10)
+	l.Title = "Tables"
+	l.SetShowHelp(false)
+
+	cols := []table.Column{
+		{Title: "url", Width: 20},
+		{Title: "status", Width: 10},
+		{Title: "latency", Width: 10},
+	}
+	rows := []table.Row{
+		{"wss://relay.damus.io", "Online", "42"},
+		{"wss://nos.lol", "Online", "85"},
+		{"wss://nostr.mom", "Online", "120"},
+	}
+	mTable := table.New(
+		table.WithColumns(cols),
+		table.WithRows(rows),
+		table.WithHeight(6),
+	)
 	return Model{
-		table: table.New(),
-		input: textarea.New(),
-		list:  list.New(emptyItems, list.NewDefaultDelegate(), 20, 10),
+		table: mTable,
+		input: ti,
+		list:  l,
 	}
 }
 
@@ -42,9 +95,59 @@ func (m Model) Init() tea.Cmd {
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.height = msg.Height
+		m.width = msg.Width
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC:
+			return m, tea.Quit
+		}
+	}
 	return m, nil
 }
 
 func (m Model) View() string {
-	return ""
+	// 1. Get the raw string outputs from the bubbles
+	listView := m.list.View()
+	inputView := m.input.View()
+	tableView := m.table.View()
+
+	listColor := inactiveBorder
+	inputColor := inactiveBorder
+	tableColor := inactiveBorder
+
+	switch m.focused {
+	case focusList:
+		listColor = activeBorder
+	case focusInput:
+		inputColor = activeBorder
+	case focusTable:
+		tableColor = activeBorder
+	}
+
+	styledList := leftColumnStyle.BorderForeground(listColor).Render(listView)
+	styledInput := rightTopStyle.BorderForeground(inputColor).Render(inputView)
+	styledTable := rightBottomStyle.BorderForeground(tableColor).Render(tableView)
+	// 4. Compose the layout
+	// Stack input and table vertically
+	rightColumn := lipgloss.JoinVertical(lipgloss.Left, styledInput, styledTable)
+
+	// Join the list and the right column horizontally
+	finalLayout := lipgloss.JoinHorizontal(lipgloss.Top, styledList, rightColumn)
+
+	// Add a little instruction text at the very bottom
+	helpText := lipgloss.NewStyle().Foreground(colorGreen).Render("\n  (Press Ctrl-C to exit. Keybindings are configurable, please see the documentation for more information.)")
+	return "\n" + finalLayout + helpText + "\n"
+}
+
+func (m *Model) Run() error {
+	p := tea.NewProgram(m, tea.WithAltScreen())
+
+	if _, err := p.Run(); err != nil {
+		return err
+	}
+
+	return nil
 }
